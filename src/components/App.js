b4be157ec65e91b5fb7ad8c2eddcf83e1896e83a
sufiYAN-87 <116790492+sufiYAN-87@users.noreply.github.com>
2023-04-02 02:05:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  let project_name  = 'Export booking/customer service portal'
+  let projectName  = 'Export booking/customer service portal'
   let disc = 'nothing for now, i will write it later...'
   return (
     <div id="main">
-    <h1 data-ns-test="project-name"> {project_name} </h1>
+    <h1 data-ns-test="project-name"> {projectName} </h1>
     <p data-ns-test="project-description"> {disc} </p>
     </div>
   )
